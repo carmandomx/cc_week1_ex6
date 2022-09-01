64091b09d5228e88b2061b0a600d6f53425b53d1
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2022-09-01 12:59:13 -0500

    feat: added instructions and test cases

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,6 +1,9 @@
-const { func } = require('../exercise');
+const { cypher } = require('../exercise');
 
-test('Testing the function exercise', () => { 
-    expect(func()).toBeDefined();
-    
+test('Testing the cypher exercise', () => { 
+    expect(cypher('')).toBeDefined();
+    expect(cypher('JRYPBZR GB VGX!')).toBe('WELCOME TO ITK!')
+    expect(cypher('PBER PBHEFR 2022')).toBe('CORE COURSE 2022')
+    expect(cypher('1 ZRQVHZ CVMMN')).toBe('1 MEDIUM PIZZA')
+    expect(cypher('GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.')).toBe('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.')
  })
--- exercise.js
+++ exercise.js
@@ -1,9 +1,21 @@
+/* *Instructions*
+    You will solve one of the most famous cypher... Caesar's cypher.
+
+    This is a shift cypher meaning that all the letters shift it's position by a set amount.
+
+    You need to implement a cypher that rotates each later exactly 13 positions (using only the English alphabeth),
+    as input you will get a coded string and you must return the decoded result.
+
+
+*/
+
 /** DO NOT CHANGE THE FUNCTION NAME **/
-const func = (c) => {
-    /* Only make changes below this comment */
-    
+const cypher = (encoded) => {
     
     /* Only make changes below this comment */
+    
+   return encoded;
+    /* Only make changes above this comment */
 }
 
 
@@ -11,4 +23,4 @@ const func = (c) => {
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
-module.exports.func = func;
+module.exports.cypher = cypher;
