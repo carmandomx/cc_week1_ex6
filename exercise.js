db66d2a2328bfbc73dbdcc6ae29cefb27646ab15
Fredy Guzman <fredy.guzman@itksquare.edu.mx>
2022-09-20 16:04:08 -0500

    Answered

--- exercise.js
+++ exercise.js
@@ -11,14 +11,50 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cypher = (encoded) => {
-    
     /* Only make changes below this comment */
-    
-   return encoded;
+    let alphabeth = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N'
+    ,'O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    const arraySize = encoded.length;
+    let decoded="";
+    // Concat the decoded message in a new string
+    for(let i=0;i<arraySize;i++){
+        decoded+=getCharValue(alphabeth,encoded[i]);
+    }
+   return decoded;
     /* Only make changes above this comment */
 }
 
-
+// Function that receives an alphabeth array and the letter that will be shifted
+// Returns the shifted letter
+const getCharValue = (arr,letter) => {
+    /* Only make changes below this comment */
+    let index=-1;
+    let arrSize= arr.length;
+    let finalLetter;
+    for(let i=0;i<arr.length;i++){
+        if(letter==arr[i]){
+            console.log(i);
+            index=i;
+        }
+    }
+    // Not a letter
+    if(index==-1){
+        finalLetter=letter;
+    } // A letter
+    else{
+        let newIndex;
+        index=index+13;
+        if(index<26){
+            newIndex= index;
+        }else{
+            newIndex= index % arrSize;
+        }
+        finalLetter= arr[newIndex];
+    }
+    
+    return finalLetter
+    /* Only make changes below this comment */
+}
 
 
 
