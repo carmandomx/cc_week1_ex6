8f264ba929efbe4e6bb2fa002af9244bf1eb24bf
Diego Velazquez <Diego.Velazquez@itksquare.edu.mx>
2022-09-22 10:04:16 -0500

    Solution

--- exercise.js
+++ exercise.js
@@ -13,7 +13,30 @@
 const cypher = (encoded) => {
     
     /* Only make changes below this comment */
-    
+    let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+    "O","P","Q","R","S","T","U","V","W","X","Y","Z"];  //We get an array with all the letters in the alphabet
+    let pos=0;
+    word=encoded.split("");
+        for(let i=0; i<word.length; i++){   //We cycle through all the elements in the array of the encoded word
+            for(let j=0; j<alphabet.length;j++){ //We cycle through all the letters in the alphabet o find wich one is equal to the word letter
+                if(word[i]===alphabet[j]){
+                    pos=j+13;
+                        if(pos>25){  //if the new position is more than the positions in the alphabet we go to the begining and start counting
+                                    //from there
+                            pos=(pos-25);
+                            pos--;
+                            }
+                        word[i]=alphabet[pos];
+                        pos=0;
+                        j=alphabet.length;
+                    }
+                }   
+            }
+    encoded="";
+        for(let i=0;i<word.length;i++){ //Here we just turn the array in an string
+                encoded += word[i];
+            }
+
    return encoded;
     /* Only make changes above this comment */
 }
