10e4f131af047b6fe02585bc86b054861940426e
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-22 21:12:32 -0500

    repo E6 Fc-encoded

--- exercise.js
+++ exercise.js
@@ -11,16 +11,95 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cypher = (encoded) => {
-    
-    /* Only make changes below this comment */
-    
-   return encoded;
-    /* Only make changes above this comment */
-}
+  /* Only make changes below this comment */
 
+  const upperCaseAlpha = [
+    0,
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
+  const lowerCaseAlpha = [
+    0,
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
+  let pivot = 13;
 
+  let arrCode = encoded.split("");
+
+  arrCode.forEach((element, index) => {
+    if (upperCaseAlpha.includes(element)) {
+      let letterU = upperCaseAlpha.indexOf(element);
+      if (letterU + pivot < upperCaseAlpha.length) {
+        arrCode[index] = upperCaseAlpha[letterU + pivot];
+      } else {
+        arrCode[index] = upperCaseAlpha[letterU - pivot];
+      }
+    }
+
+    if (lowerCaseAlpha.includes(element)) {
+      let letterL = lowerCaseAlpha.indexOf(element);
+      if (letterL + pivot < lowerCaseAlpha.length) {
+        arrCode[index] = lowerCaseAlpha[letterL + pivot];
+      } else {
+        arrCode[index] = lowerCaseAlpha[letterL - pivot];
+      }
+    }
+  });
+  encoded = arrCode.join("").toUpperCase();
+  return encoded;
+  /* Only make changes above this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.cypher = cypher;
