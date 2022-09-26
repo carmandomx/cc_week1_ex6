9be7740e86df29ea1ca7b0a5aa70ab91dec6d230
Genaro Arredondo <Genaro.Arredondo@itksquare.edu.mx>
2022-09-26 01:18:14 -0500

    Solved the sixth exercise

--- exercise.js
+++ exercise.js
@@ -13,8 +13,21 @@
 const cypher = (encoded) => {
     
     /* Only make changes below this comment */
-    
-   return encoded;
+    let decoded = '';                           //Create an empty string to store the decoded string
+    for (i = 0; i < encoded.length; i++) {      //Create a loop to iterate through every character from the string
+        let letter = encoded.charCodeAt(i);     //Use charCodeAt to get the current character's HTML ASCII unicode 
+        if (letter >= 65 && letter <= 90) {     //Create a conditional to only pass true when the character is a letter from the alphabet, all other characters stay the same
+        for(j = 0; j < 13; j++) {               //Create a loop to cycle 13 times
+            if (letter == 90)                   //Check if the unicode is for the letter Z
+            letter -= 25;                       //If true, subtract 25 to position the unicode at letter A (65)
+            else {
+         letter += 1;                           //if false, add 1 to the unicode
+            }
+        }
+    }
+        decoded += String.fromCharCode(letter); //After cycling the character unicode 13 times, get the letter with the character code and add it to the empty string
+    }
+    return decoded;                             // return the decoded string
     /* Only make changes above this comment */
 }
 
