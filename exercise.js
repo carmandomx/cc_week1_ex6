3c255b4fa2a864f7dced65661b0f47b8d4e9b847
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-03-03 12:26:02 -0600

    solved

--- exercise.js
+++ exercise.js
@@ -11,13 +11,26 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cypher = (encoded) => {
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] /*we define the english alphabet */
+    code_length=encoded.length  /* Length of the code. Important value for loops*/
+    let encoded_arr=encoded.split("")  /*split de string to array cause array are unmutable and hard to manipulate */
+    for (let i=0;i<code_length;i++){  /*iterate through the whole code */
+        for (let j=0;j<26;j++){  /* iterate through alphabet*/
+            if (encoded[i]===alphabet[j]){  /* We check is a string element is in the english alphabet and which position it is in*/
+                encoded_arr[i]=alphabet[(j+13)%26] /* we rotate it cyclicaly 13 places through the english alphabet*/
+                break  /* break the loop is string element actually in the alphabet*/
+            }
+        }
+
+    }
+    encoded='' /* auxiliar to contain solution*/
+    for (let i=0;i<code_length;i++){  /* we stick back the splitted encoding after rotating its elements*/
+        encoded+=encoded_arr[i]
+    }
     
-    /* Only make changes below this comment */
     
    return encoded;
-    /* Only make changes above this comment */
 }
-
 
 
 
