495eac52b4fa106d06e626fcecb0246b47ab72f9
Andrei Santos <Andrei.Santos@itksquare.edu.mx>
2023-03-05 13:25:31 -0600

    cc_week1_ex6 AndreiSantos

--- exercise.js
+++ exercise.js
@@ -13,8 +13,22 @@
 const cypher = (encoded) => {
     
     /* Only make changes below this comment */
+    let abc = ["A","B","C","D","E","F","G","H","I",
+        "J","K","L","M","N","O","P","Q","R","S","T",
+        "U","V","W","X","Y","Z"];
+
+    let decoded = "";
+    encoded.split('').forEach((element) => { // split the string to convert it in an array
+        let index = 0;
+        let pos = 0;
+        if (abc.includes(element)) { // asking if the element is a letter of the abc array
+            index = abc.indexOf(element); // taking and saving the index of the element on the abc array
+            pos = index + 13 > 25 ? index - 13 : index + 13; // rotating 13 positions to the raight on the abc array
+        }
+        decoded += abc.includes(element) ? abc[pos] : element; // decodify the letter and save it in a new string
+    });
     
-   return encoded;
+   return decoded;
     /* Only make changes above this comment */
 }
 
